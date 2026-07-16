b7ae9e852a0b932544afcfe3f9b680d8a27bb97e
Songqiao Han <92891855+hansq358@users.noreply.github.com>
2026-07-16 05:10:17 +0100

    Delete Simulation_Code/Additional_Experiments/Sequential_vs_Joint.R

--- Simulation_Code/Additional_Experiments/Sequential_vs_Joint.R
+++
@@ -1,5 +0,0 @@
-# Sequential vs joint D-optimal design benchmark, Case 3 (Supplementary Table S15).
-# Small scale (R = 20, N = 1000) because the joint Fedorov search is expensive.
-# Run from this folder:  Rscript Sequential_vs_Joint.R
-source("common.R")
-run_sequential()
